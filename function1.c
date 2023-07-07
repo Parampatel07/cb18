1794623cb6c0636e81a27f4db6e743841e7a924d
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-07-07 10:02:56 +0530

    Add files via upload

---
+++ function1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void printline( )
+{
+    printf("----------------------------------------\n");
+
+}
+
+void main()
+{
+    printline( );
+    printf(" name : sahil \n");
+    printline();
+    printf(" age  : 18 \n");
+    printline();
+    printf(" weight : 45.36 \n");
+    printline();
+}
--- temp.c
+++ temp.c
@@ -1,74 +1,46 @@
-#include<stdio.h>
- void main()
- {
+#include <stdio.h>
+void main()
+{
 
-     int marks[5][5],temp,flash;
+    int marks[5][5], temp, flash, avg[5], j;
 
-     for (flash = 0; flash <= 4;flash++)
-     {
-         printf("Enter marks of stude %d\n",flash+1);
-         for (temp = 0; temp <= 4; temp++)
-         {
-             printf(" Enete marks of subject %d ",temp+1);
-             scanf("%d", &marks[0][temp]);
-         }
-   }
+    for (flash = 0; flash <= 4; flash++)
+    {
+        printf("Enter marks of stude %d\n", flash + 1);
+        for (temp = 0; temp <= 4; temp++)
+        {
+            printf(" Enete marks of subject %d ", temp + 1);
+            scanf("%d", &marks[flash][temp]);
+        }
+    }
+    for (j = 0; j <= 4; j++)
+    {
+        avg[j] = (marks[j][0] + marks[j][1] + marks[j][2] + marks[j][3] + marks[j][4]) / 5;
+        printf(" avg of student %d is %d \n", (j + 1), avg[j]);
+    }
 
-
- }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (avg[0] > avg[1] && avg[0] > avg[2] && avg[0] > avg[3] && avg[0] > avg[4])
+    {
+        printf(" student 1 have highest rank ");
+    }
+    else if (avg[1] > avg[0] && avg[1] > avg[2] && avg[1] > avg[3] && avg[1] > avg[4])
+    {
+        printf(" student 2 have highest rank ");
+    }
+    else if (avg[2] > avg[0] && avg[2] > avg[1] && avg[2] > avg[3] && avg[2] > avg[4])
+    {
+        printf(" student 3 have highest rank ");
+    }
+    else if (avg[3] > avg[0] && avg[3] > avg[1] && avg[3] > avg[2] && avg[3] > avg[4])
+    {
+        printf(" student 4 have highest rank ");
+    }
+    else if (avg[4] > avg[0] && avg[4] > avg[1] && avg[4] > avg[2] && avg[4] > avg[3])
+    {
+        printf(" student 5 have highest rank ");
+    }
+    printf("good bye.......");
+}
 
 /*
 #include <stdio.h>
